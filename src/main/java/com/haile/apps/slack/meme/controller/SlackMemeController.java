d47efe64dc063266f4a028a22bd694aafa63dae9
Hailemichael <hailemichael.nigatu@gmail.com>
2018-07-27 16:35:44 +0200

    MediaType changed to MediaType.APPLICATION_FORM_URLENCODED

--- src/main/java/com/haile/apps/slack/meme/controller/SlackMemeController.java
+++ src/main/java/com/haile/apps/slack/meme/controller/SlackMemeController.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,9 +26,11 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 public class SlackMemeController {
 	private static final Logger logger = LoggerFactory.getLogger(SlackMemeController.class);
 	
-	@RequestMapping(value = "/meme", method = RequestMethod.POST, consumes="application/x-www-form-urlencoded", produces="application/json")
-	public @ResponseBody ResponseEntity<?> memefyImage(HttpServletRequest request, @RequestParam HashMap<String, Object> body){
+	@RequestMapping(value = "/meme", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED, produces=MediaType.APPLICATION_JSON)
+	public @ResponseBody ResponseEntity<?> memefyImage(HttpServletRequest request, @RequestBody HashMap<String, Object> body){
 		logger.info("Incomming request: " + request.getServletPath() + "_" + request.getRemoteAddr() + "_" + request.getRemoteUser());
+		logger.info("Content-Type: " + request.getHeader("Content-Type") + " or " + request.getContentType() + " Encoding: " + request.getCharacterEncoding());
+		logger.info("Response url: " + body.get("response_url"));
 		ObjectMapper mapper = new ObjectMapper();		
 		try {
 			logger.info(mapper.writeValueAsString(body));
@@ -69,7 +73,7 @@ public class SlackMemeController {
 		
 		String jsonString = null;
 		try {
-			jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(output);
+			jsonString = mapper.writeValueAsString(output);
 			logger.info("Output: " + jsonString);
 		} catch (JsonProcessingException e) {
 			new ResponseEntity<> ("Couldn't generate output!", HttpStatus.INTERNAL_SERVER_ERROR);
@@ -78,9 +82,12 @@ public class SlackMemeController {
 		return new ResponseEntity<> (jsonString, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/meme/confirm", method = RequestMethod.POST, consumes="application/x-www-form-urlencoded", produces="application/json")
-	public @ResponseBody ResponseEntity<?> confirmPost(HttpServletRequest request, @RequestParam HashMap<String, Object> body){
+	@RequestMapping(value = "/meme/confirm", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED, produces=MediaType.APPLICATION_JSON)
+	public @ResponseBody ResponseEntity<?> confirmPost(HttpServletRequest request, @RequestBody HashMap<String, Object> body){
 		logger.info("Incomming request: " + request.getServletPath() + "_" + request.getRemoteAddr() + "_" + request.getRemoteUser());
+		logger.info("Content-Type: " + request.getHeader("Content-Type") + " or " + request.getContentType() + " Encoding: " + request.getCharacterEncoding());
+		HashMap<String, Object> payload = (HashMap<String, Object>) body.get("payload");
+		logger.info("response_url: " + ((payload != null)? payload.get("response_url"):"payload cannot be found"));
 		ObjectMapper mapper = new ObjectMapper();		
 		try {
 			logger.info(mapper.writeValueAsString(body));
@@ -99,7 +106,7 @@ public class SlackMemeController {
 		output.put("attachments", attachments);
 		String jsonString = null;
 		try {
-			jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(output);
+			jsonString = mapper.writeValueAsString(output);
 			logger.info("Output: " + jsonString);
 		} catch (JsonProcessingException e) {			
 			new ResponseEntity<> ("Couldn't generate output!", HttpStatus.INTERNAL_SERVER_ERROR);
